bd5c959b1cac6c33b4326dca4786a006699cc418
JINHYUNGP <carpedm96@naver.com>
2023-07-07 00:29:03 +0900

    [Fix] 기존의 string 정제 방식을 extension으로 추출

---
+++ Movie_Practice/Movie_Practice/Extensions/String + SpecialCharacters.swift
@@ -0,0 +1,21 @@
+//
+//  String + removingSpecialCharactersAndWhitespace.swift
+//  Movie_Practice
+//
+//  Created by 박진형 on 2023/07/06.
+//
+
+import Foundation
+
+extension String {
+    func removeLeadingCommaSpace() -> String {
+        var modifiedString = self
+        let leadingCommaSpace = ", "
+        
+        if modifiedString.hasPrefix(leadingCommaSpace) {
+            modifiedString.removeFirst(leadingCommaSpace.count)
+        }
+        
+        return modifiedString
+    }
+}
